1821c784b347d41534ab10956bc1759eb10932d6
Matthew Quigley <matthew.quigley@and.digital>
2023-01-26 18:39:15 +0000

    edit: made tests pass

--- Tests.py
+++ Tests.py
@@ -8,7 +8,7 @@ def test_answer():
   assert func(3) == 4
 
 def test_answer_2():
-  assert func(3) == 5
+  assert func(4) == 5
 
 def test_hello_world(capfd):
   assert HelloWorld.main() == "Hello World!"
